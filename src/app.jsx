ced75d9a31aefece819d3c6c9b080c194b303d49
Krystal Campioni <krystal.designer@gmail.com>
2016-11-15 23:05:07 -0200

    get and log current location

--- src/app.jsx
+++ src/app.jsx
@@ -4,6 +4,12 @@ import axios from 'axios';
 
 export default class App extends React.Component {
   componentWillMount() {
+  navigator.geolocation.getCurrentPosition(function(location) {
+    console.log(location.coords.latitude);
+    console.log(location.coords.longitude);
+    console.log(location.coords.accuracy);
+  });
+
   axios.get('http://api.openweathermap.org/data/2.5/weather?q=sao_paulo&units=metric&APPID=0ecce192500d1a71eb05d1ef815147cc')
     .then(function (response) {
       console.log(response);
